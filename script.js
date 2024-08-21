86d66af8b5fec01c2213f8e593e1ca19448a92f3
Albieri Alaña <166877458+Albieri151@users.noreply.github.com>
2024-08-20 22:16:45 -0400

    Add files via upload
    Signed-off-by: Albieri Alaña <166877458+Albieri151@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+function validateText(text) {
+    // Verifica si el texto contiene mayúsculas o caracteres especiales
+    const regex = /^[a-z\s]*$/;  // Solo acepta letras minúsculas y espacios
+    return regex.test(text);
+}
+
+function showAlert(message) {
+    alert(message);  // Muestra una alerta con el mensaje
+}
+
 function encryptText() {
     let text = document.getElementById("input-text").value;
+
+    if (!validateText(text)) {
+        showAlert("Solo se permiten letras minúsculas y sin caracteres especiales.");
+        return;
+    }
+
     let encryptedText = text
         .replace(/e/g, "enter")
         .replace(/i/g, "imes")
@@ -11,6 +27,12 @@ function encryptText() {
 
 function decryptText() {
     let text = document.getElementById("input-text").value;
+
+    if (!validateText(text)) {
+        showAlert("Solo se permiten letras minúsculas y sin caracteres especiales.");
+        return;
+    }
+
     let decryptedText = text
         .replace(/enter/g, "e")
         .replace(/imes/g, "i")
